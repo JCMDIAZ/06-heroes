6bc5bc7c1ef8840b60e648e8ef51d781b4fed1d2
Carlos Manuel Díaz <33435012+JCMDIAZ@users.noreply.github.com>
2024-06-03 15:14:12 -0600

    Avance Seccion 14

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesServices } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+  public heroes: Hero[] = [];
 
+  constructor( private heroesService: HeroesServices) {}
+
+  ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe(
+      data => this.heroes = data
+    )
+  }
 }
