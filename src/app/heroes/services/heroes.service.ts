9712c2b05397c6ede689a1cc5339319a64f1c448
Carlos Manuel Díaz <33435012+JCMDIAZ@users.noreply.github.com>
2024-06-06 12:15:00 -0600

    Fin de la sesión15

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { env } from '../../../env/env.prod';
 
@@ -13,4 +13,22 @@ export class HeroesServices {
     getHeroes(): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
     }
+
+    getHeroById(id: string):Observable<Hero|undefined> {
+        return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+            .pipe(
+                catchError( error => of(undefined))
+            );
+    }
+
+    getSuggestions( query: string ): Observable<Hero[]> {
+        return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+    } // No funciono el json-serve nueva version
+
+    /*getSuggestions(query: string): Observable<Hero[]> {
+        return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+            .pipe(
+                map(heroes => heroes.filter(hero => hero.superhero.toLowerCase().includes(query.toLowerCase())))
+            );
+    }*/ // Nueva version de json-serve
 }
